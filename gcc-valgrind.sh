54f188ee023329b05080a00d2749a2683c32a25b
Kai Tamkun <ktamkun@ucsc.edu>
2023-09-09 23:29:00 -0700

    Add valgrind scripts

---
+++ gcc-valgrind.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+g++ -std=c++20 -Wall -Wextra -g -O0 $1 -o /tmp/$(basename "$1").x && valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all /tmp/$(basename "$1").x ${@:2}
+rm -f /tmp/$(basename "$1").x
